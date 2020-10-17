1e0931e8a386ce6836f9328a56f9b8041c260c4d
Samaritan89 <237732937@qq.com>
2020-10-17 17:32:50 +0800

    fix: 添加注释

--- test/checkbox-with-label.test.js
+++ test/checkbox-with-label.test.js
@@ -5,13 +5,16 @@ import Adapter from 'enzyme-adapter-react-16';
 import CheckboxWithLabel from '../src/checkbox-with-label';
 
 beforeAll(() => {
+  // enzyme 初始化
   Enzyme.configure({ adapter: new Adapter() });
 })
 
 test('CheckboxWithLabel changes the text after click', () => {
+  // 渲染组件
   const checkbox = shallow(<CheckboxWithLabel labelOn="On" labelOff="Off" />);
   expect(checkbox.text()).toEqual('Off');
 
+  // 触发事件
   checkbox.find('input').simulate('change');
   expect(checkbox.text()).toEqual('On');
 });
