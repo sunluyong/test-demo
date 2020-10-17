1c4b2c69c3daf25201b8094c6deaf615d013904c
Samaritan89 <237732937@qq.com>
2020-10-17 15:39:59 +0800

    fix: 添加外层 describe 包裹

--- test/async.test.js
+++ test/async.test.js
@@ -1,29 +1,31 @@
 const { cb, pm, aa } = require('../src/async');
 
-test('callback data is peanut butter', done => {
-  function callback(data) {
-    try {
-      expect(data).toBe('peanut butter');
-      done();
-    } catch (error) {
-      done(error);
+describe('异步函数测试', () => {
+  test('callback data is peanut butter', done => {
+    function callback(data) {
+      try {
+        expect(data).toBe('peanut butter');
+        done();
+      } catch (error) {
+        done(error);
+      }
     }
-  }
 
-  cb(callback);
-});
+    cb(callback);
+  });
 
-test('promise then data is peanut butter', () => {
-  return pm().then(data => {
+  test('promise then data is peanut butter', () => {
+    return pm().then(data => {
+      expect(data).toBe('peanut butter');
+    });
+  });
+
+  test('promise resolve data is peanut butter', () => {
+    return expect(pm()).resolves.toBe('peanut butter');
+  });
+
+  test('async/await data is peanut butter', async () => {
+    const data = await aa();
     expect(data).toBe('peanut butter');
   });
-});
-
-test('promise resolve data is peanut butter', () => {
-  return expect(pm()).resolves.toBe('peanut butter');
-});
-
-test('async/await data is peanut butter', async () => {
-  const data = await aa();
-  expect(data).toBe('peanut butter');
 });
